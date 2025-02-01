acdf417949164ccd2a32cf8416ed7cb596dce83e
AriLeGrand <mrivierez@gaming.tech>
2025-02-01 17:58:53 +0100

    #start threading

--- main.cpp
+++ main.cpp
@@ -2,12 +2,8 @@
 #include <SFML/Graphics.hpp>
 #include "network.h"
 
-
-
-int main()
+int ChooseMode()
 {
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "Online - TicTacToe");
     // Ask user if they want to run as server or client
     std::cout << "Run as (s)erver or (c)lient? ";
     char choice;
@@ -19,7 +15,6 @@ int main()
         return 0;
     }
     else if (choice == 'c' || choice == 'C') {
-        
         std::cout << "Run as client." << std::endl;
         RunClient();
         return 1;
@@ -27,4 +22,38 @@ int main()
     else {
         std::cout << "Invalid choice. Exiting." << std::endl;
     }
+    return 1;
+}
+
+int game()
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "Online - TicTacToe");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    return 1;
+}
+
+int main()
+{
+    std::thread thread_B(game);
+    std::thread thread_A(ChooseMode);
+
+    thread_B.join();
+    thread_A.join();
+
+    return 0;
 }
